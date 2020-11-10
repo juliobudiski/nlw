7c998a4183edfc07cf07d0fd106fcf5a0e430d49
juliobudiski <juliobudiski@users.noreply.github.com>
2020-11-10 00:18:41 -0300

    Finish second day

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,27 +1,26 @@
-const map = L.map("mapid").setView([-22.2490295,-53.3514348], 15);
-
+const map = L.map("mapid").setView([-22.2490295, -53.3514348], 15);
 
 //create map
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-//crrate icon
+//create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58,68],
-    iconAnchor: [29,68],
-    popupAnchor: [170,2]
-})
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
-// create popup overlay
-const poup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar Alternativo São José <a href="orphanage.html?id=1" class="choose-orphange"> <img src="./public/images/arrow-white.svg"<a/>')
+//create popup overlay
+const popup = L.popup({
+  closeButton: false,
+  className: "map-popup",
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Lar Alternativo São José <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>'
+);
 
-// create and add marker
-L.marker([-22.2504677,-53.3447375], {icon})
-  .addTo(map)
-  .bindPopup(popup)
+//create and add marker
+L.marker([-22.2504677, -53.3447375], { icon }).addTo(map).bindPopup(popup);
